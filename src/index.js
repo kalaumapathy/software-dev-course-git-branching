968e4daad95254c0b8b8cba09b86a0331e2ece78
kalaumapathy <50033847+kalaumapathy@users.noreply.github.com>
2026-08-27 12:43:58 -0500

    Log square of numbers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // Kala Umapathy - 08/27/2026
+//This application prints "Hello, World!" and the numbers 0 to 9, along with their squares.
 
 console.log("Hello world!");
 
 for (let i = 0; i < 10; i++) {
     console.log(i);
+    console.log(i*i);
 }
